3bdab35bde5a00427bfae2ac1c2d189f4f7df87c
Piti-14 <alupit526@ieselcaminas.org>
2023-10-31 17:36:47 +0100

    App Sol actualizada, revisar DropDownMenu de las Cards y añadir SnackBar y ModalDrawer (menu lateral)

--- app/src/main/java/com/example/sol/MainActivity.kt
+++ app/src/main/java/com/example/sol/MainActivity.kt
@@ -1,19 +1,47 @@
 package com.example.sol
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.sol.ui.theme.SolTheme
 import androidx.navigation.NavHost
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
@@ -23,26 +51,68 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    val navController = rememberNavController()
-                    NavHost()
+                    MyApp()
+
                 }
             }
         }
     }
 }
-
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
+@Preview
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp() {
+    Scaffold(
+        bottomBar = { ToolBar() },
+
+    ){
+        Content()
+    }
 }
 
-@Preview(showBackground = true)
+//@Preview
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GreetingPreview() {
-    SolTheme {
-        Greeting("Android")
+fun ToolBar() {
+    BottomAppBar(
+        containerColor = Color.Red
+    ){
+        FAB()
     }
+}
+
+//@Preview
+@Composable
+fun FAB() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(10.dp),
+        contentAlignment = Alignment.BottomEnd
+    ) {
+        var context = LocalContext.current
+        Button(
+            onClick = { Toast.makeText(context, "Hola!", Toast.LENGTH_SHORT).show() },
+            modifier = Modifier.clip(CircleShape),
+        ) {
+            Image(painter = painterResource(id = R.drawable.ic_more), contentDescription = "")
+        }
+    }
+}
+@Preview
+@Composable
+fun Content() {
+    LazyVerticalGrid(
+        columns = GridCells.Fixed(2),
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+        horizontalArrangement = Arrangement.spacedBy(8.dp)
+    ){
+        val images = getSolarImages()
+        items(images.size){
+            GenerateSolarCard(image = images[it])
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/sol/MainPage.kt
+++ app/src/main/java/com/example/sol/MainPage.kt
@@ -1,7 +1,10 @@
 package com.example.sol
 
 import androidx.compose.foundation.ExperimentalFoundationApi
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.grid.GridCells
@@ -10,16 +13,29 @@ import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
 import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material3.BottomAppBar
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CardElevation
+import androidx.compose.material3.Divider
+import androidx.compose.material3.DropdownMenu
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -38,19 +54,41 @@ fun BottomBar() {
     }
 }
 
-@OptIn(ExperimentalFoundationApi::class)
 @Composable
-fun ShowSolarImages() {
-    LazyVerticalGrid(
-        columns = GridCells.Fixed(2)
-    ){
-        item { Card(
-            modifier = Modifier.fillMaxWidth(),
-            elevation = CardDefaults.cardElevation(12.dp)
-        ) {
+fun GenerateSolarCard(image: SolarImage) {
+    Card(
+        modifier = Modifier.fillMaxSize(),
 
-        } }
+        ) {
+        Image(
+            painter = painterResource(id = image.route),
+            contentDescription = image.name
+        )
+
+        Divider()
+        Row (
+            horizontalArrangement = Arrangement.End
+        ){
+            var state  by rememberSaveable { mutableStateOf(false) }
+            Text(text = image.name, style = TextStyle(fontSize = 14.sp, fontWeight = FontWeight.Bold))
+
+            DropdownMenu(expanded = state , onDismissRequest = { state = !state }) {
+                Image(painter = painterResource(id = R.drawable.triple_dots), contentDescription = "dotsmenu")
+            }
+        }
+
     }
 }
 
-fun GetImages(){}
+fun getSolarImages(): List<SolarImage>{
+    return listOf(
+        SolarImage("Corona Solar", R.drawable.corona_solar),
+        SolarImage("Erupción Solar", R.drawable.erupcionsolar),
+        SolarImage("Espículas", R.drawable.espiculas),
+        SolarImage("Filamentos", R.drawable.filamentos),
+        SolarImage("Magnetosfera", R.drawable.magnetosfera),
+        SolarImage("Mancha Solar", R.drawable.manchasolar),
+    )
+}
+
+data class SolarImage(val name: String, val route: Int)
