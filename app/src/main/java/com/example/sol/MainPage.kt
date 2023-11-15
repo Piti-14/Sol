0c1ae5dda5364f31ce7e5677dacb4c5c04ef4fb3
Piti-14 <alupit526@ieselcaminas.org>
2023-11-15 20:10:54 +0100

    Implementar DropDownMenu y hacer que el scroll funcione correctamente

--- app/src/main/java/com/example/sol/MainPage.kt
+++ app/src/main/java/com/example/sol/MainPage.kt
@@ -1,74 +1,45 @@
 package com.example.sol
 
-import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.lazy.grid.GridCells
-import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
-import androidx.compose.foundation.lazy.grid.items
-import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
-import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.MoreVert
-import androidx.compose.material3.BottomAppBar
-import androidx.compose.material3.Button
+import androidx.compose.material.icons.filled.Share
 import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CardElevation
-import androidx.compose.material3.Divider
 import androidx.compose.material3.DropdownMenu
-import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.ComposableInferredTarget
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
-@OptIn(ExperimentalMaterial3Api::class)
-@Composable
-fun DisplayMainPage() {
-    Scaffold (
-        topBar = { BottomBar() },
-        content = { it }
-    )
-}
+data class SolarImage(val name: String, val route: Int)
 
-
-@Composable
-fun BottomBar() {
-    BottomAppBar {
-        Text(text = "hola")
-    }
-}
 
 @Composable
 fun GenerateSolarCard(image: SolarImage) {
+    var showDropDownMenu by rememberSaveable {
+        mutableStateOf(false)
+    }
+
     Card(
         Modifier.height(250.dp)
     )
@@ -93,10 +64,36 @@ fun GenerateSolarCard(image: SolarImage) {
                 style = TextStyle(fontSize = 18.sp, fontWeight = FontWeight.Bold),
             )
 
-            IconButton(onClick = { /*TODO*/ }) {
+            IconButton(onClick = { showDropDownMenu = true }) {
                 Icon(imageVector = Icons.Default.MoreVert, contentDescription = "Options")
+
+                DropDownMenu()
             }
         }
+    }
+}
+
+@Composable
+fun DropDownMenu(showDropDownMenu: Boolean) {
+    var show by rememberSaveable {
+        mutableStateOf(showDropDownMenu)
+    }
+
+    DropdownMenu(
+        expanded = show,
+        onDismissRequest = { show = false }
+    ) {
+        DropdownMenuItem(
+            text = { Text("Compartir") },
+            onClick = { /*TODO*/ },
+            leadingIcon = { Icon(imageVector = Icons.Default.Share, contentDescription = "Compartir")}
+        )
+
+        DropdownMenuItem(
+            text = { Text("Eliminar") },
+            onClick = { /*TODO*/ },
+            leadingIcon = { Icon(imageVector = Icons.Default.Delete, contentDescription = "Eliminar")}
+        )
     }
 }
 
@@ -110,5 +107,3 @@ fun getSolarImages(): List<SolarImage>{
         SolarImage("Mancha Solar", R.drawable.manchasolar),
     )
 }
-
-data class SolarImage(val name: String, val route: Int)
